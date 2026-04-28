7246084a4039b06669f46345884b293b25529723
David Sh <7243697+David-EIPI@users.noreply.github.com>
2026-04-28 11:46:41 -0400

    Fix zb_float32/int conversions, add ppm->int conversion

--- firmware/myzcl/zb_float.h
+++ firmware/myzcl/zb_float.h
@@ -5,10 +5,18 @@
     Below is the function to convert integer data to binary representation of single precision float
     compatible with ZCL standard types.
  */
-typedef struct {
+typedef union {
     uint32_t v;
+    float f;
 } zb_float32_t;
 
+/*
+union _zb_float32_type_conversion_helper_u {
+    uint32_t u;
+    float f;
+};
+*/
+#define ZB_FLOAT32_FROM_FLOAT(f) (((union _zb_float32_type_conversion_helper_u)(f)).u)
 
 /**@brief Convert integer measurement value in PPM units (parts per million) to
   single precision floating point fractional value (that is the output value is the input x 1e-6) */
@@ -23,7 +31,7 @@ void concentration_ppm_to_float(const uint32_t ppm, zb_float32_t *out)
     PPM values without 32-bit overflow. The largest PPM value that can be safely converted
     is therefore 2^18 = 262144.
 */
-    uint32_t a = (ppm * (1 << 14)) / 15625;
+    uint32_t a = (ppm * (1 << 14) + (15625 / 2)) / 15625;
 
     int32_t upper_bit = a ? 31 - __builtin_clz(a) : 0;
 /* Exponent part of the IEE754 float */
@@ -34,58 +42,99 @@ void concentration_ppm_to_float(const uint32_t ppm, zb_float32_t *out)
     out->v = (x << 23) | (m << (23 - upper_bit));
 }
 
+/**@brief Convert measurement value in PPM units (parts per million) in FP32 format to
+  integer PPM value (this is needed for validation). */
+inline static
+int32_t concentration_float_to_ppm(const zb_float32_t *val)
+{
+    uint32_t a = val->v;
+
+    int sign = a & 0x80000000;
+
+    int32_t x = ((a >> 23) & 0xff) - 0x7f + 20;
+
+    if (x > 31) {
+        return sign + (0x80000000u - 1u);
+    }
+
+    if (x < 0)
+        return 0;
+
+    uint32_t m = a & ((1 << 23) - 1);
+
+    m |= (1 << 23);
+
+    int32_t result;
+    if (x > 23) {
+        result = m << (x - 23);
+    } else {
+        result = m >> (23 - x);
+    }
+
+    if (sign)
+        result = - result;
+
+    return (result * 15625 + (1<<13)) / (1 << 14);
+}
+
 /**@brief Convert integer measurement value to single precision floating point integer value. */
 inline static
 void int32_to_float(const int32_t val, zb_float32_t *out)
 {
-    int32_t a = (val * (1 << 14)) / 15625;
+    int32_t a = val;
     int sign = a < 0;
     if (sign) a = -a;
 
     int32_t upper_bit = a ? 31 - __builtin_clz(a) : 0;
+
 /* Exponent part of the IEE754 float */
     uint32_t x = a ? (0x80 ^ (unsigned char)(upper_bit - 1)) : 0;
 /* Mantissa */
     uint32_t m = a ? (a & ((1 << upper_bit) - 1)) : 0;
+
+/* Truncate mantissa if needed */
+    if (upper_bit > 23) {
+        m >>= upper_bit - 23;
+        upper_bit = 23;
+    }
+
 /* Build final output value */
     out->v = (x << 23) | (m << (23 - upper_bit)) | (sign << 31);
 }
 
+
 /**@brief Convert integer floating point to signed integer type (fractional part is discarded). */
 inline static
-void float_to_int32(const zb_float32_t *val, int32_t *out)
+int32_t float_to_int32(const zb_float32_t *val)
 {
     uint32_t a = val->v;
 
-    int sign = a & (1<<31);
+    int sign = a & 0x80000000;
 
-    uint32_t x = (((a >> 23) & 0xff) ^ 0x80) + 1;
+    int32_t x = ((a >> 23) & 0xff) - 0x7f;
+
     if (x > 31) {
-        *out = sign + (0x80000000u - 1u);
-        return;
+        return sign + (0x80000000u - 1u);
     }
+
+    if (x < 0)
+        return 0;
+
     uint32_t m = a & ((1 << 23) - 1);
-    int32_t upper_bit = m ? 31 - __builtin_clz(m) : 0;
 
-    if (0 == m) {
-        if (0 == x) {
-            *out = 0;
-            return;
-        } else {
-            m = 1;
-        }
-    } else {
-        m |= (1 << (upper_bit + 1));
-    }
+    m |= (1 << 23);
 
-    if (x > upper_bit) {
-        *out = m << (x - upper_bit - 1);
+    int32_t result;
+    if (x > 23) {
+        result = m << (x - 23);
     } else {
-        *out = m >> (upper_bit - x + 1);
+        result = m >> (23 - x);
     }
 
     if (sign)
-        *out = - *out;
+        result = - result;
+
+    return result;
 }
 
 
